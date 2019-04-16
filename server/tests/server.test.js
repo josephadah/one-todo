dea9e570f358b0fc6c7471d2d4704f42c0a3832e
Joseph Jonathan <jonathanadahjoe@gmail.com>
2019-04-16 01:06:10 +0100

    configure for heroku

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo.model');
 
 const todos = [
-    {text: 'First todo for test'},
-    {text: 'Second todo for test'}
+    {_id: new ObjectID(), text: 'First todo for test'},
+    {_id: new ObjectID(), text: 'Second todo for test'}
 ];
 
 beforeEach(done => {
@@ -66,6 +67,32 @@ describe('GET todos tests', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/id', () => {
+    it('Should get a todo by id', (done) => {
+        request(app)
+            .get(`/api/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.text).toBe(todos[0].text)
+            })
+            .end(done);
+    });
+
+    it('Should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/api/todos/${new ObjectID()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('Should return 404 for non object ids', (done) => {
+        request(app)
+            .get(`/api/todos/123`)
+            .expect(404)
             .end(done);
     });
 });
